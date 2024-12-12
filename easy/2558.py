c77a923a1d351fa562566b052420b28cf2912e5d
rykovlad <vlad.rikov.2002@gmail.com>
2024-12-12 20:07:45 +0200

    2558 daily ez

---
+++ easy/2558.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        while k > 0:
+            gifts.sort()
+            gifts[-1] = int(gifts[-1]**0.5)
+            print(gifts)
+            k += -1
+        res = sum(gifts)
+        print(res)
+        return res
+
+
+
+s  = Solution()
+s.pickGifts([25,64,9,4,100], 4)
